ca353cd45e12acf45dbc155e48b70e6b5f2bfe55
Routsis Michail <mike7199@icloud.com>
2025-04-14 20:05:04 +0300

    updates

--- logger/logger.js
+++ logger/logger.js
@@ -29,6 +29,10 @@
 //   transports: [new transports.Console()]
 // })
 
+
+//For jest tests
+// require('dotenv').config()
+
 // Third Example
 require('winston-daily-rotate-file');
 require('winston-mongodb');
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,7 +5,7 @@ const userController = require('../controllers/user.controller');
 const verifyToken = require('../middlewares/auth.middleware').verifyToken
 const verifyRoles = require('../middlewares/auth.middleware').verifyRoles
 
-router.get('/', verifyToken,  userController.findAll);
+router.get('/', verifyToken , userController.findAll);
 router.get('/:username', userController.findOne);
 router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create);
 router.patch('/:username', verifyToken, verifyRoles("ADMIN"), userController.update);
