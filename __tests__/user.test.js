c73c90dbd5d8215a28f4ee72f5d5c87e8bcf2443
Routsis Michail <mike7199@icloud.com>
2025-04-15 19:03:29 +0300

    updates

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -43,6 +43,86 @@ describe("Requests for /api/users", ()=>{
     expect(res.body.status).toBeTruthy();
     expect(res.body.data.length).toBeGreaterThan(0);
   }, 50000);
+
+  it("POST Creates a user", async ()=>{
+    const res = await request(app)
+      .post('/api/users')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        'username':'test5',
+        'password':'12345',
+        'name':'test5 name',
+        'surname': 'test5 surname',
+        'email':'test5@aueb.gr',
+        'address': {
+          'area':'area1',
+          'road':'road5'
+        }
+      });
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.status).toBeTruthy();
+  }, 50000)
+
+  it("POST Creates a user that exists", async()=>{
+    const res = await request(app)
+      .post('/api/users')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        username: 'test5',
+        password:'12345',
+        name: 'new name',
+        surname:'new surname',
+        email:'new@aueb.gr',
+        address: {
+          area: 'xxxx',
+          road: 'yyyy'
+        }
+      })
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.status).not.toBeTruthy()
+  }) 
+
+  it("Post Creates a user with same email", async () => {
+    const res = await request(app)
+      .post('/api/users')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        username: 'test6',
+        password: '12345',
+        name: 'name test6',
+        surname: 'surname test6',
+        email: 'test5@aueb.gr',
+        address: {
+          area: 'area23',
+          road: 'road23'
+        }
+      })
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.status).not.toBeTruthy();
+   })
+
+   it('POST Creates a user with empty name, surnmame, password', async () => {
+      const res = await request(app)
+        .post('/api/users')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          username: 'test6',
+          password: '',
+          name : '',
+          surname: '',
+          email: 'test6@aueb.gr',
+          address : {
+            area: 'area23',
+            road: 'road23'
+          }
+        });
+
+        expect(res.status).toBe(400)
+        expect(res.body.status).not.toBeTruthy();
+   })
 });
 
 
@@ -61,7 +141,6 @@ describe("Requests for /api/users/:username", () => {
   it("Get returns specific user", async()=>{
     
     const result = await userService.findLastInsertedUser();
-    console.log("RESULT>>", result);
     
     const res = await request(app)
       .get('/api/users/'+result.username)
